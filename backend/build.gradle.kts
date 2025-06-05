7c33767b6501718047511a8a035f515953b24a1a
Jowiies <joel.esteller@estudiantat.upc.edu>
2025-06-05 12:11:20 +0200

    kotlin backend using ktor

---
+++ backend/build.gradle.kts
@@ -0,0 +1,29 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.1.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+}
+
+group = "com.cafeteria.epsevg"
+version = "0.0.1"
+
+application {
+    mainClass = "com.cafeteria.epsevg.ApplicationKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
